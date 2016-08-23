e6f31f5d4dbf6bcab3619a64a2a30c8089e3c06c
Alexey Kravtsov <ka@70.ru>
2016-08-23 17:48:29 +0300

    ignore git

--- Tests/TestAuthData.cs
+++ Tests/TestAuthData.cs
@@ -2,7 +2,7 @@
 {
     internal static class TestAuthData
     {
-        public const string Username = "username";
+        public const string Username = "kravcov_alexey@mail.ru";
         public const string Password = "password";
     }
 }
